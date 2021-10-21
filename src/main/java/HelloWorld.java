d08d3edc4ba4c1f2aeaa74e0c70049813cbf4891
pravakar15031986 <68266118+pravakar15031986@users.noreply.github.com>
2021-10-21 09:53:03 +0530

    added3

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "CSM Technology Bhubaneswar");
+		get("/hello", (req, res) -> "CSM Technology Bhubaneswar is the fastest growing company");
 	}
 }
